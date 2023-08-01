488cb7c60db90fbaeaafffdc65886b9f765462e7
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-08-01 15:10:16 +0700

    setting template dir

--- product_management/settings.py
+++ product_management/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "product_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

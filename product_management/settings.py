bf3051ab6a0c51e2124825593dff77f17e93d1c5
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-08-07 17:22:07 +0700

    add setting to display image

--- product_management/settings.py
+++ product_management/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "file_upload")  # file_upload adalah nama folder
+MEDIA_URL = "/media/"  # media adalah nama url
